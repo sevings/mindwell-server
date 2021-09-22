01d198cc2204c81213f363ef4008e7494ffc756a
binque <binque@ya.ru>
2021-09-22 19:41:27 +0300

    -

--- scripts/update.sql
+++
@@ -1,8 +0,0 @@
-DROP FUNCTION mindwell.can_view_tlog(INTEGER, TEXT);
-DROP FUNCTION mindwell.can_view_tlog(INTEGER);
-DROP FUNCTION mindwell.can_view_tlog(INTEGER, INTEGER);
-DROP FUNCTION mindwell.can_view_entry(INTEGER, INTEGER);
-DROP FUNCTION mindwell.can_view_entry(INTEGER, INTEGER, INTEGER, TEXT);
-
-ALTER TABLE mindwell.entries
-ADD COLUMN "is_commentable" Boolean DEFAULT TRUE NOT NULL;
