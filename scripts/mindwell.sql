84c0135a936b153b9543b8822f900d30673c8e68
sevings <binque@ya.ru>
2023-02-02 22:25:28 +0100

    fix give_invites

--- scripts/mindwell.sql
+++ scripts/mindwell.sql
@@ -2407,44 +2407,56 @@ CREATE INDEX "index_user_log_name" ON "mindwell"."user_log" USING btree( "name" 
 
 
 CREATE OR REPLACE FUNCTION mindwell.give_invites() RETURNS TABLE(user_id int) AS $$
-    WITH inviters AS (
-        UPDATE mindwell.users 
+WITH inviters AS (
+    UPDATE mindwell.users
         SET last_invite = CURRENT_DATE
         WHERE ((id IN (
-                    SELECT author_id 
-                    FROM (
-                        SELECT created_at, author_id, rating 
-                        FROM mindwell.entries
-                        WHERE age(created_at) <= interval '1 month' 
-                            AND visible_for = (SELECT id FROM mindwell.entry_privacy WHERE type = 'all')
-                        ORDER BY rating DESC 
-                        LIMIT 100) AS e
-                    WHERE created_at::date = CURRENT_DATE - 3
-                )
-                AND (SELECT COUNT(*) FROM mindwell.invites WHERE referrer_id = users.id) < 3
-            ) OR (
-                last_invite = created_at::Date
-                AND (
-                    SELECT COUNT(DISTINCT entries.id)
-                    FROM mindwell.entries, mindwell.entry_votes
-                    WHERE entries.author_id = users.id AND entry_votes.entry_id = entries.id
-                        AND entry_votes.vote > 0 AND entry_votes.user_id <> users.invited_by
-                ) >= 10
-            )) AND age(last_invite) >= interval '14 days'
+            SELECT user_id
+            FROM (
+                     SELECT entries.created_at, user_id
+                     FROM mindwell.entries
+                              JOIN mindwell.users ON user_id = users.id
+                     WHERE age(entries.created_at) <= interval '1 month'
+                       AND visible_for in (
+                         SELECT id
+                         FROM mindwell.entry_privacy
+                         WHERE type in ('all', 'registered', 'invited')
+                     )
+                       AND users.invited_by IS NOT NULL
+                       AND users.privacy in (
+                         SELECT id
+                         FROM mindwell.user_privacy
+                         WHERE type in ('all', 'registered', 'invited')
+                     )
+                     ORDER BY rating DESC
+                     LIMIT 100) AS e
+            WHERE current_timestamp - e.created_at < interval '3 days'
+        )
+            AND (SELECT COUNT(*) FROM mindwell.invites WHERE referrer_id = users.id) < 3
+                   ) OR (
+                           last_invite = created_at::Date
+                       AND (
+                               SELECT COUNT(DISTINCT entries.id)
+                               FROM mindwell.entries
+                                        JOIN mindwell.entry_votes ON entries.id = entry_votes.entry_id
+                               WHERE entries.user_id = users.id
+                                 AND entry_votes.vote > 0
+                                 AND entry_votes.user_id <> users.invited_by
+                           ) >= 10
+                   )) AND age(last_invite) >= interval '14 days'
             AND invite_ban <= CURRENT_DATE
-            AND invited_by IS NOT NULL
         RETURNING users.id
-    ), wc AS (
-        SELECT COUNT(*) AS words FROM mindwell.invite_words
-    )
-    INSERT INTO mindwell.invites(referrer_id, word1, word2, word3)
-        SELECT inviters.id, 
-            trunc(random() * wc.words),
-            trunc(random() * wc.words),
-            trunc(random() * wc.words)
-        FROM inviters, wc
-        ON CONFLICT (word1, word2, word3) DO NOTHING
-    RETURNING referrer_id;
+), wc AS (
+    SELECT COUNT(*) AS words FROM mindwell.invite_words
+)
+INSERT INTO mindwell.invites(referrer_id, word1, word2, word3)
+SELECT inviters.id,
+       trunc(random() * wc.words),
+       trunc(random() * wc.words),
+       trunc(random() * wc.words)
+FROM inviters, wc
+ON CONFLICT (word1, word2, word3) DO NOTHING
+RETURNING referrer_id;
 $$ LANGUAGE SQL;
 
 
--- scripts/update.sql
+++ scripts/update.sql
@@ -1,55 +1,52 @@
-ALTER TABLE user_log
-ADD COLUMN uid2 Bigint NOT NULL DEFAULT 0;
-
-ALTER TABLE user_log
-ALTER COLUMN uid2 DROP DEFAULT;
-
-DROP VIEW user_log_view;
-
-CREATE VIEW "mindwell"."user_log_view" AS
-SELECT name, ip, to_hex(device) AS device, to_hex(app) AS app, to_hex(uid) AS uid, to_hex(uid2) AS uid2,
-       to_char(at, 'YYYY.MM.DD HH24:MI:SS') AS at, first AS f
-FROM mindwell.user_log
-ORDER BY at DESC;
-
-ALTER TABLE users
-ADD COLUMN "user_ban" Date DEFAULT CURRENT_DATE NOT NULL;
-
-DROP FUNCTION ban_user(username text);
-
-CREATE OR REPLACE FUNCTION mindwell.delete_user(user_name TEXT) RETURNS VOID AS $$
-DECLARE
-    del_id INTEGER;
-BEGIN
-    del_id = (SELECT id FROM users WHERE lower(name) = lower(user_name));
-
-    DELETE FROM mindwell.relations WHERE to_id = del_id;
-    DELETE FROM mindwell.relations WHERE from_id = del_id;
-
-    DELETE FROM mindwell.favorites WHERE favorites.user_id = del_id;
-    DELETE FROM mindwell.watching WHERE watching.user_id = del_id;
-    DELETE FROM mindwell.entries_privacy WHERE entries_privacy.user_id = del_id;
-
-    DELETE FROM mindwell.entry_votes WHERE entry_votes.user_id = del_id;
-    DELETE FROM mindwell.comment_votes WHERE comment_votes.user_id = del_id;
-    DELETE FROM mindwell.vote_weights WHERE vote_weights.user_id = del_id;
-
-    DELETE FROM mindwell.notifications
-    WHERE notifications.user_id = del_id OR
-        CASE (SELECT "type" FROM notification_type WHERE notification_type.id = notifications."type")
-            WHEN 'comment' THEN
-                    (SELECT user_id FROM comments WHERE comments.id = notifications.subject_id) = del_id
-            WHEN 'invite' THEN
-                FALSE
-            ELSE
-                    notifications.subject_id = del_id
-            END;
-
-    DELETE FROM complains WHERE user_id = del_id;
-
-    DELETE FROM mindwell.images WHERE images.user_id = del_id;
-    DELETE FROM mindwell.entries WHERE user_id = del_id;
-    DELETE FROM mindwell.comments WHERE user_id = del_id;
-    DELETE FROM mindwell.users WHERE id = del_id;
-END;
-$$ LANGUAGE plpgsql;
+CREATE OR REPLACE FUNCTION mindwell.give_invites() RETURNS TABLE(user_id int) AS $$
+WITH inviters AS (
+    UPDATE mindwell.users
+        SET last_invite = CURRENT_DATE
+        WHERE ((id IN (
+            SELECT user_id
+            FROM (
+                     SELECT entries.created_at, user_id
+                     FROM mindwell.entries
+                     JOIN mindwell.users ON user_id = users.id
+                     WHERE age(entries.created_at) <= interval '1 month'
+                       AND visible_for in (
+                         SELECT id
+                         FROM mindwell.entry_privacy
+                         WHERE type in ('all', 'registered', 'invited')
+                       )
+                       AND users.invited_by IS NOT NULL
+                       AND users.privacy in (
+                         SELECT id
+                         FROM mindwell.user_privacy
+                         WHERE type in ('all', 'registered', 'invited')
+                       )
+                     ORDER BY rating DESC
+                     LIMIT 100) AS e
+            WHERE current_timestamp - e.created_at < interval '3 days'
+        )
+            AND (SELECT COUNT(*) FROM mindwell.invites WHERE referrer_id = users.id) < 3
+                   ) OR (
+                           last_invite = created_at::Date
+                       AND (
+                               SELECT COUNT(DISTINCT entries.id)
+                               FROM mindwell.entries
+                               JOIN mindwell.entry_votes ON entries.id = entry_votes.entry_id
+                               WHERE entries.user_id = users.id
+                                 AND entry_votes.vote > 0
+                                 AND entry_votes.user_id <> users.invited_by
+                           ) >= 10
+                   )) AND age(last_invite) >= interval '14 days'
+            AND invite_ban <= CURRENT_DATE
+        RETURNING users.id
+), wc AS (
+    SELECT COUNT(*) AS words FROM mindwell.invite_words
+)
+INSERT INTO mindwell.invites(referrer_id, word1, word2, word3)
+SELECT inviters.id,
+       trunc(random() * wc.words),
+       trunc(random() * wc.words),
+       trunc(random() * wc.words)
+FROM inviters, wc
+ON CONFLICT (word1, word2, word3) DO NOTHING
+RETURNING referrer_id;
+$$ LANGUAGE SQL;
